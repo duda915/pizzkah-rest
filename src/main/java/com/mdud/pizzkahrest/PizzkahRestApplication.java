4f7c2a563ed34f0cdfce0f6b83918ee148c2d9b4
MDud <duda915@gmail.com>
2018-08-15 21:34:35 +0200

    Initial spring commit

---
+++ src/main/java/com/mdud/pizzkahrest/PizzkahRestApplication.java
@@ -0,0 +1,12 @@
+package com.mdud.pizzkahrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzkahRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzkahRestApplication.class, args);
+	}
+}
